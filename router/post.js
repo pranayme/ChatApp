9aa0dfe809044071a0a968fd824a97915920f862
pranayme <82631893+pranayme@users.noreply.github.com>
2023-10-12 15:20:22 +0530

    Update post.js

--- router/post.js
+++ router/post.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const authController = require('../controller/auth') // Import authentication controller
-
+const authController = require('../controller/auth') 
 const router = express.Router()
 
-// POST requests
+
 router.post('/sign-up', authController.signUp)
 router.post('/sign-in', authController.signIn)
 router.post('/sign-out', authController.signOut)
